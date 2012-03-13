a9c13839e49257fef1a9598d0d5c02749b5bb95c
CY Lee <m.cylee@gmail.com>
2012-03-13 06:10:19 +0000

    Part 1 and Part 2 a, b Add: /Given the following movies exist/ in movie_steps.rb Add: Rottenpotatoes home page to features/support/path.rb Add: /Given I check the following ratings/ in movies_steps.rb Add: Scenario "restrict movies with PG or G ratings

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  assert false, "Unimplmemented"
+  # assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +26,7 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/\s*,\s*/).each do |rating|
+    step %{I #{uncheck ? 'un' : ''}check "ratings_#{rating}"}
+  end
 end
